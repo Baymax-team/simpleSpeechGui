d0c836240c94563526736c7c3e1d88a7d0ab448c
hatem zamzam <hatem.zamzam@gmail.com>
2018-12-05 21:31:59 +0200

    added recognition for commands-speech

--- gui_without_timer.py
+++ gui_without_timer.py
@@ -1,8 +1,16 @@
-from threading import Thread
 from tkinter import *
 from time import *
 import pyaudio
 import wave
+from keras.models import load_model
+import librosa
+import numpy as np
+import pickle
+import os
+
+classes = list(pickle.load(open('labels_classes.pkl', 'rb')))
+model = load_model('weights_2/model-ep016-loss0.077-val_loss0.929.h5')
+maxLength = 32
 
 class App:
     
@@ -54,12 +62,31 @@ class App:
             self.root.after(100,self.endRecord)
         else:
             self.root.after(100,self.record)
+    
+    def recognize(self):
+        wave, sr = librosa.load(self.WAVE_OUTPUT_FILENAME,mono = True, sr = None)
+        mfcc = librosa.feature.mfcc(wave, sr = 16000)
+        padWidth = maxLength - mfcc.shape[1]
+        mfcc = np.pad(mfcc,pad_width = ((0, 0), (0, padWidth)), mode = "constant")
+        
+        mfcc1 = np.reshape(np.array(mfcc), (1, 20, 32, 1))
+        
+        pred = model.predict(mfcc1)
+#        print(np.max(pred))
+        pred = int(pred.argmax(axis=-1))
+        print(classes[pred])
+        self.userResponse.configure(text=str(classes[pred]))
+        if os.path.exists(self.WAVE_OUTPUT_FILENAME):
+            os.remove(self.WAVE_OUTPUT_FILENAME)
+        else:
+          print("The file does not exist")
 
     def endRecord(self):
         print("{} saved".format(self.WAVE_OUTPUT_FILENAME))
         self.timeDisp.configure(text="{} saved".format(self.WAVE_OUTPUT_FILENAME))
         self.recordBtn.configure(state=NORMAL)
         self.timeDisp.configure(text="")
+        self.root.after(100,self.recognize)
 
     def record(self):
         
